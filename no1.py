c703ef7c81aa9d6d166d82a21d0356640354218e
Ryzz99 <160326789+Ryzz99@users.noreply.github.com>
2024-05-10 11:04:39 +0700

    no1.py

--- no1.py
+++ no1.py
@@ -1,7 +1,7 @@
-numbers = [8, 3, 12, 4, 7, 2]
+numbers = [8, 3, 12, 0, 7, 2]
 
 for i in range(len(numbers)):
-    if numbers[i] < 4:
+    if numbers[i] < 5:
         numbers[i] = 0
 
 numbers.sort(reverse=True)
